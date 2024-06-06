54a9465ebbdeda3d06fd3153a1db5395bce6dff4
Aluno <Aluno@10.85.198.41>
2024-06-06 14:19:07 -0300

    Commit project Casting

---
+++ src/Casting01.java
@@ -0,0 +1,28 @@
+public class Casting01 {
+    public static void main(String[] args) {
+        //Declare and initialize a byte with a value of 128
+        
+     short myshot = 128;
+     
+     short mybyte = 128;
+             
+    short shortTobyte =(byte) 127;
+    
+    byte value =(byte) ((byte) 127 + 1);
+    
+    System.out.println(value);
+     
+     
+     
+     
+        //Declare and initialize a short with a value of 128
+        //Create a print statement that casts this short to a byte
+        //Declare and initialize a byte with a value of 127
+        //Add 1 to this variable and print it
+        //Add 1 to this variable again and print it again
+
+        
+        
+        
+    }    
+}
